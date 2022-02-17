93fd52abbd24ae5136adc8974c43bca809fbe04f
yohannNDB <91455596+yohannNDB@users.noreply.github.com>
2022-02-17 11:06:15 +0100

    Update projet_Titanic.py

--- projet_Titanic.py
+++ projet_Titanic.py
@@ -44,7 +44,8 @@ image = PhotoImage(file = "fond_papier.gif")
 #création de l'image de fond et du Titre
 canvas_pour_image = Canvas(window,width=700,height=450, bg='white',bd=0,highlightthickness=0)
 canvas_pour_image.create_image(0,0,image = image,anchor='nw')
-canvas_pour_image.create_text(20,150,text="Bienvenue dans l'application de statistiques du Titanic",font=("Script",35),anchor='nw')
+canvas_pour_image.create_text(20,150,text="Bienvenue dans l'applicationde statistiques du Titanic",\
+                             font=("Script",35),anchor='nw')
 canvas_pour_image.pack(fill='both',expand=True)
 
 #(fonction en cours)weight_au_harsard_pour_bulles = random.randint(0,200)
@@ -55,21 +56,26 @@ def bulle():
     while ok_pour_bulle==True:
         for i in range (1):
             #image des bulles
-            image_de_la_bulle= PhotoImage(file = "bulle_fond.gif",width=110,height=300)
-            image_de_la_petite_bulle= PhotoImage(file = "bulle_fond.gif",width=110,height=100)
+            image_de_la_bulle= PhotoImage(file = "bulle_fond.gif",\
+                             width=110,height=300)
+            image_de_la_petite_bulle= PhotoImage\
+                    (file = "bulle_fond.gif",width=110,height=100)
 
 
 
             #creation de trois canvas différents pour acceuillir les bulle 1,2,3
-            bulle = canvas_pour_image.create_image(0,450,image= image_de_la_bulle,anchor='nw')
+            bulle = canvas_pour_image.create_image\
+                    (0,450,image= image_de_la_bulle,anchor='nw')
             canvas_pour_image.pack(fill='both',expand=True)
 
 
-            bulle2 = canvas_pour_image.create_image(500,450,image= image_de_la_bulle,anchor='nw')
+            bulle2 = canvas_pour_image.create_image\
+                    (500,450,image= image_de_la_bulle,anchor='nw')
             canvas_pour_image.pack(fill='both',expand=True)
 
             #cette bulle utilise une image différente mais est toujours placé à un endroit voulu
-            bulle3 = canvas_pour_image.create_image(150,450,image= image_de_la_petite_bulle,anchor='nw')
+            bulle3 = canvas_pour_image.create_image\
+                    (150,450,image= image_de_la_petite_bulle,anchor='nw')
             canvas_pour_image.pack(fill='both',expand=True)
 
             #déplacement de la bulle 1
@@ -102,7 +108,8 @@ def bulle():
 image_bouton = PhotoImage(file="bouton_fond2.gif")
 
 data = pd.read_excel('titanic3.xls')
-data = data.drop(['name', 'sibsp', 'parch', 'ticket', 'fare', 'cabin', 'embarked', 'boat', 'body', 'home.dest'],axis=1)
+data = data.drop(['name', 'sibsp', 'parch', 'ticket',\
+                'fare', 'cabin', 'embarked', 'boat', 'body', 'home.dest'],axis=1)
 data.shape
 
 # fonction qui additione toutes les variables
@@ -136,10 +143,10 @@ def filtres():
     if oui_homme.get() == 1:
         if total_variables() == 1:
             total_lignes = total_lignes - len(data[data['sex'] == 'female'])
-            
+
     return total_lignes
-            
-    
+
+
 def afficher():
     print (filtres())
     print (total_variables())
@@ -172,35 +179,62 @@ def CreateNewWindow():
     window.resizable(width=True,height=True)
 
 # caractéritiques du pseudo titre des statistiques
-    label_frame_options = Label(window,text="Les critères",font=("Courrier",30),bg='#048B9A',fg='white')
+    label_frame_options = Label(window,text="Les critères",\
+                        font=("Courrier",30),bg='#048B9A',fg='white')
 
 # caracteristiques des titre des themes des options
 
-    label_frame_theme1 = Label(window,text="- Le sexe des passagers",font=("Courrier",25),bg='#048B9A',fg='white')
-    label_frame_theme2 = Label(window,text="- La classe des passagers",font=("Courrier",25),bg='#048B9A',fg='white')
-    label_frame_theme3 = Label(window,text="- L'age des passagers",font=("Courrier",25),bg='#048B9A',fg='white')
-    label_frame_theme4 = Label(window,text="- Survivant",font=("Courrier",25),bg='#048B9A',fg='white')
+    label_frame_theme1 = Label(window,text="- Le sexe des passagers",\
+                        font=("Courrier",25),bg='#048B9A',fg='white')
+    label_frame_theme2 = Label(window,text="- La classe des passagers",\
+                        font=("Courrier",25),bg='#048B9A',fg='white')
+    label_frame_theme3 = Label(window,text="- L'age des passagers",\
+                        font=("Courrier",25),bg='#048B9A',fg='white')
+    label_frame_theme4 = Label(window,text="- Survivant",\
+                        font=("Courrier",25),bg='#048B9A',fg='white')
 #caractéristiques des options du theme 1
-    case_theme1_option1 = Radiobutton(window,text ='Homme',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_homme,value=1)
-    case_theme1_option2 = Radiobutton(window,text ='Femme',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_homme,value=2)
+    case_theme1_option1 = Radiobutton(window,text ='Homme',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_homme,value=1)
+    case_theme1_option2 = Radiobutton(window,text ='Femme',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_homme,value=2)
 
 #caractéristiques des options du theme 2
 
-    case_theme2_option1 = Checkbutton(window,text ='1ère Classe',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_1ere_classe)
-    case_theme2_option2 = Checkbutton(window,text ='2ème Classe',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_2ere_classe)
-    case_theme2_option3 = Checkbutton(window,text ='3ème Classe',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_3ere_classe)
+    case_theme2_option1 = Checkbutton(window,text ='1ère Classe',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_1ere_classe)
+    case_theme2_option2 = Checkbutton(window,text ='2ème Classe',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',
+                        activebackground='#048B9A',variable=oui_2ere_classe)
+    case_theme2_option3 = Checkbutton(window,text ='3ème Classe',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_3ere_classe)
 
 #caractéristiques des options du theme 3
-    case_theme3_option1 = Checkbutton(window,text ='- de 18 ans',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_moins_de_18)
-    case_theme3_option2 = Checkbutton(window,text =' 18 à 50 ans',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_de_18_a_50)
-    case_theme3_option3 = Checkbutton(window,text =' plus de 50 ans',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_plus_de_50ans)
+    case_theme3_option1 = Checkbutton(window,text ='- de 18 ans',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_moins_de_18)
+    case_theme3_option2 = Checkbutton(window,text =' 18 à 50 ans',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_de_18_a_50)
+    case_theme3_option3 = Checkbutton(window,text =' plus de 50 ans',bg='#048B9A',\
+                        fg='black',font=("Courrier",15),activeforeground='white',\
+                        activebackground='#048B9A',variable=oui_plus_de_50ans)
 
 #caractéristiques des options du theme 4
-    case_theme4_option1 = Radiobutton(window,text ='Oui',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_Oui_survivant,value=1)
-    case_theme4_option2 = Radiobutton(window,text ='Non',bg='#048B9A',fg='black',font=("Courrier",15),activeforeground='white',activebackground='#048B9A',variable=oui_Oui_survivant,value=2)
+    case_theme4_option1 = Radiobutton(window,text ='Oui',bg='#048B9A',fg='black',\
+                        font=("Courrier",15),activeforeground='white',
+                        activebackground='#048B9A',variable=oui_Oui_survivant,value=1)
+    case_theme4_option2 = Radiobutton(window,text ='Non',bg='#048B9A',fg='black',\
+                        font=("Courrier",15),activeforeground='white',
+                        activebackground='#048B9A',variable=oui_Oui_survivant,value=2)
 #caractéristiques du bouton "rechercher"
 
-    button_rechercher = Button(window,text='rechercher',bg='#048B9A',fg='white',font=("Courrier",20),activeforeground='white',activebackground='#048B9A',command=afficher)
+    button_rechercher = Button(window,text='rechercher',bg='#048B9A',fg='white',\
+                        font=("Courrier",20),activeforeground='white',\
+                        activebackground='#048B9A',command=afficher)
 
 
 
@@ -256,8 +290,10 @@ canvas.grid(row=0,column=0)
 """
 
 #boutton "commencer l'aventure" ansi que son placement dans la fenêtre
-button1 = Button(window,image= image_bouton,borderwidth=0,highlightthickness=0,command=CreateNewWindow)
-my_button1_window = canvas_pour_image.create_window(255,450/2,anchor = "nw",window=button1)
+button1 = Button(window,image= image_bouton,borderwidth=0,\
+                               highlightthickness=0,command=CreateNewWindow)
+my_button1_window = canvas_pour_image.create_window\
+                    (255,450/2,anchor = "nw",window=button1)
 
 
 
